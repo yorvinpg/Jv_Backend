6401c9b48e65fc2ba8eb7ad295872cd3b36babd0
YorivnPG <56844361+yorvinpg@users.noreply.github.com>
2022-06-29 09:37:27 -0500

    Proyecto Backend

---
+++ src/main/java/com/example/jv_backend/JvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jv_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvBackendApplication.class, args);
+	}
+
+}
